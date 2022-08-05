a8adbbd03d3f333b401fc3f1b2bad4df9b0ea736
Nilesh030 <97057349+Nilesh030@users.noreply.github.com>
2022-08-05 11:42:45 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+
+const server = http.createServer(app);
+
+ 
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+
+const io = new Server(server, {
+  cors: {
+    origin:"*",
+  methods: ['GET','POST','DELETE','UPDATE','PUT'],
+  allowedHeaders:['Content-Type', 'Authorization','sessionId'],
+  exposedHeaders:['sessionId'],
+  preflightContinue:false
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER RUNNING");
+});
